46a9983384aaf71f07d8b8096876f8baf5f4de11
LilLongg <glivingstone70@gmail.com>
2025-12-02 13:32:55 +0700

    Update image cache when changed

--- home/.config/niri/scripts/swaybg_blur.sh
+++ home/.config/niri/scripts/swaybg_blur.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/fish
 
-set -l BLUR_CACHE_PATH "$HOME/.cache/wpp/$argv[1]_blured.JPG"
-if not test -f "$BLUR_CACHE_PATH"
-    set -l CACHE_IMG "$HOME/.cache/wpp/$argv[1].JPG"
+set -l IMG $argv[1]
+set -l BLUR_CACHE_PATH "$HOME/.cache/wpp/"$IMG"_blured.JPG"
+
+if test "$IMG" -nt "$BLUR_CACHE_PATH"
+    set -l CACHE_IMG "$HOME/.cache/wpp/$IMG.JPG"
     magick convert -scale 10% -blur 0x1.0 -resize 1000% "$CACHE_IMG" "$BLUR_CACHE_PATH"
 end
 
