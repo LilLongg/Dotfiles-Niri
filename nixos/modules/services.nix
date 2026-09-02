709521b4012464a52f75c49203475eb82ad0ae19
LilLongg <glivingstone70@gmail.com>
2026-09-03 00:11:09 +0700

    Test services

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -9,9 +9,12 @@
       package = pkgs.ananicy-cpp;
       rulesProvider = pkgs.ananicy-rules-cachyos;
     };
+    auto-cpufreq.enable = true;
     tlp = {
       enable = true;
       settings = {
+        TLP_DISABLE_DEFAULTS = 1;
+        TLP_AUTO_SWITCH = 0;
         RESTORE_THRESHOLDS_ON_BAT = 1;
         START_CHARGE_THRESH_BAT1 = 0;
         STOP_CHARGE_THRESH_BAT1 = 60;
